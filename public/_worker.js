d1650ba1326f0a951068242e10eabc4ce970609e
DreamCaptureEnt <dreamcapture2025@gmail.com>
2026-05-27 10:52:32 +0530

    worker.j

--- public/_worker.js
+++ public/_worker.js
@@ -6,14 +6,22 @@ export default {
 
     if (url.pathname.startsWith('/api/')) {
       const backendUrl = new URL(url.pathname + url.search, BACKEND_ORIGIN);
-      const proxyRequest = new Request(backendUrl, request);
+      const proxyRequest = new Request(backendUrl.toString(), request);
 
       proxyRequest.headers.set('Host', new URL(BACKEND_ORIGIN).host);
       proxyRequest.headers.set('X-Forwarded-Proto', 'https');
 
-      return fetch(proxyRequest);
+      const response = await fetch(proxyRequest);
+      const proxyResponse = new Response(response.body, response);
+      proxyResponse.headers.set('X-Bleen-Proxy', 'cloudflare-pages-worker');
+      return proxyResponse;
     }
 
-    return env.ASSETS.fetch(request);
+    const assetResponse = await env.ASSETS.fetch(request);
+    if (assetResponse.status !== 404) {
+      return assetResponse;
+    }
+
+    return env.ASSETS.fetch(new Request(new URL('/index.html', url), request));
   },
 };
